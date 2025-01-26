1830c660790065cf9d089214ce144be81d9e74c6
abd alsayad <134537357+abdsyd@users.noreply.github.com>
2025-01-26 14:36:59 +0300

    finished members page

--- src/pages/members/members.jsx
+++ src/pages/members/members.jsx
@@ -1,94 +1,33 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import profilePic from './card_pic.png';
+import { BiPhoneCall } from "react-icons/bi";
+import { FaUserTie } from "react-icons/fa6";
+import './members.css'
+
 
 export default function Members() {
     return (
-        <>
-        <h1>الوكلاء</h1>
-        <div className="container">
-            <div className="row m-4">
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </div>
-            <div className="row m-4">
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="col-4">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+        <div className='members'>
+            <h1>الوكلاء</h1>
+            <div className="container">
+                <div className="row g-4">
+                    {Array.from({ length: 6 }).map((_, index) => (
+                        <div key={index} className="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+                            <Card>
+                                <Card.Img src={profilePic} />
+                                <Card.Body>
+                                <Card.Title className="fw-bold fs-4 pb-4">عهد سليمان</Card.Title>
+                                    <Card.Text> <FaUserTie />
+                                    الرئيس التنفيذي</Card.Text>
+                                    <Card.Text>
+                                    <BiPhoneCall />+963 996 840 955
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-        </>
     );
 }
